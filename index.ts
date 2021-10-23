7ce4705d057139101c56d30fc4b17dd5d8956da6
Craig Meinschein <cmeinsch@gmail.com>
2021-10-23 00:34:55 -0700

    Add initial bot framework
    Add discord.js, create a client which successfully logs into discord,
    given a token.

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hello, world")
+require("dotenv").config()
+const token = process.env.TOKEN
+if (!token) {
+  throw new Error("TOKEN must be provided as an environment variable")
+}
+import { Client, Intents } from "discord.js"
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client!.user!.tag}!`)
+})
+
+client.login(token)
