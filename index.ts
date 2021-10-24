d04e8eb76c8f2535c4f3ef39bd56694257dc8ed6
Craig Meinschein <cmeinsch@gmail.com>
2021-10-23 20:06:30 -0700

    Add express webserver

--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 require("dotenv").config()
 const token = process.env.TOKEN
+const port = process.env.PORT || 8080
 if (!token) {
   throw new Error("TOKEN must be provided as an environment variable")
 }
 import { Client, Intents } from "discord.js"
+import express from "express"
+const web = express()
+web.get("/", function (req, res) {
+  res.send("")
+})
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-
 client.on("ready", () => {
   console.log(`Logged in as ${client.user!.tag}!`)
 })
-
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return
 
@@ -32,4 +36,6 @@ client.on("interactionCreate", async (interaction) => {
 })
 
 client.login(token)
+web.listen(port)
 // todo check out https://www.npmjs.com/package/discord.js.test
+// todo create a dev bot
