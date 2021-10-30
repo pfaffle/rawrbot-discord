40101e5cdf8d6dff8e5512476c5cd3c94fee0864
Craig Meinschein <cmeinsch@gmail.com>
2021-10-29 19:01:08 -0700

    Load commands only once

--- src/discord.ts
+++ src/discord.ts
@@ -3,11 +3,12 @@ import { logger } from "./logger"
 import { loadCommands } from "./register-commands"
 
 export const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const commands = loadCommands()
+
 client.on("ready", () => {
   logger.info(`Logged in as ${client.user!.tag}!`)
 })
 client.on("interactionCreate", async (interaction) => {
-  const commands = loadCommands()
   if (!interaction.isCommand()) return
   const { commandName } = interaction
   const command = commands.find((command) => command.def.name === commandName)
